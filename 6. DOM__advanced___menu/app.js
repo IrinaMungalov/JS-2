53998356b0ef7be1b7e9d8afb6e5b297ea5e2660
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-01-22 10:55:42 +0200

    Add files via upload

--- 6. DOM__advanced___menu/app.js
+++ 6. DOM__advanced___menu/app.js
@@ -1,6 +1,7 @@
 // DATA SCHEMA
 class MenuItem {
-    constructor (name, image, price) {
+    constructor (id, name, image, price) {
+        this.id = id
         this.name = name
         this.image = image
         this.price = price
@@ -18,18 +19,58 @@ class Drink extends MenuItem {
 let data = {
     menu: {
         food: [
-            new Food("Pizza", "images/pizza.jpg", 100),
-            new Food("Salad", "images/salad.jpg", 50),
-            new Food("Soup", "images/soup.jpeg",  25),
+            new Food(1, "Pizza", "images/pizza.jpg", 100),
+            new Food(2, "Salad", "images/salad.jpg", 50),
+            new Food(3, "Soup", "images/soup.jpeg",  25),
         ],
-        drinks: [
-            // HW1: do the same for drinks
-            new Food("Black Tea", "", 50),
-            new Food("Green Tea", "", 45),
-            new Food("Water",     "", 15),
+        drinks: [           
+            new Drink(4, "Black Tea", "images/blackTea.jpg", 50),
+            new Drink(5, "Green Tea", "images/greenTea.jpg", 45),
+            new Drink(6, "Water",     "images/water.jpg", 15),
         ]
+    },
+    order: {
+        items: [],
+        total: 0
+    },
+    promotions: {
+        freeDeliveryLimit: {
+            limit: 200,
+            cost: 0,
+            deliveryCost: 50,
+        },
+        discount: {
+            limit: 5,
+            cost: 0.9
+        }
     }
 }
+
+
+const addItemToOrder = (id) => {
+    let selectedItem = data.menu.food.find( 
+        item => item.id === id
+    )
+    data.order.items.push(
+        {itemRef: selectedItem, quantity: 1}
+    )
+    data.order.total += selectedItem.price
+    renderOrder(footerSection, data.order)
+}
+
+const removeItemFromOrder = (id) => {
+    const itemsCount = data.order.items.length
+
+    for (let i = 0; i < itemsCount; i++) {
+        if (data.order.items[i].itemRef.id === id) {
+            data.order.items.splice(i, 1)
+            return
+        }
+    }
+}
+
+
+
 
 
 // RENDER
@@ -37,10 +78,16 @@ const renderMenuItem = (menuItem, parentElement, handler) => {
         let label = document.createElement('label')
         let input = document.createElement('input')       
             input.setAttribute("type", "checkbox")
+            input.setAttribute('data-id', menuItem.id)
         let labelText = document.createTextNode(`${menuItem.name} ${menuItem.price}`)
 
+        let img = document.createElement('img')
+        img.src = menuItem.image
+
         label.appendChild(input)
+        label.appendChild(img)
         label.appendChild(labelText)
+
         parentElement.appendChild(label)
 
         // bind event handling
@@ -69,7 +116,7 @@ const renderMenuForm = (rootElement, menu) => {
     let drinkSet = renderItemSet('Drinks')
 
     menu.food.forEach( foodData => renderMenuItem(foodData, foodSet, toggleMenuItemHandler))
-    menu.food.forEach( foodData => renderMenuItem(foodData, drinkSet, toggleMenuItemHandler))
+    menu.drinks.forEach( drinkData => renderMenuItem(drinkData, drinkSet, toggleMenuItemHandler))
 
     form.appendChild(foodSet)    
     form.appendChild(drinkSet)
@@ -77,16 +124,20 @@ const renderMenuForm = (rootElement, menu) => {
     rootElement.appendChild(form)    
 }
 
-const renderMenuQuantity = (decHandler, incHandler) => {
+const renderMenuItemQuantity = (id, decHandler, incHandler) => {
     let div = document.createElement('div')
     let btnDec = document.createElement('button')
         btnDec.addEventListener('click', decHandler)
         btnDec.innerText = "-"
+        btnDec.setAttribute('data-id', id)
     let inputQ = document.createElement('input')
         inputQ.value = 1
+        inputQ.type = "text"
+        inputQ.setAttribute('data-id', id)
     let btnInc = document.createElement('button')
         btnInc.addEventListener('click', incHandler)
         btnInc.innerText = "+"
+        btnInc.setAttribute('data-id', id)
 
     div.appendChild(btnDec)
     div.appendChild(inputQ)
@@ -95,38 +146,121 @@ const renderMenuQuantity = (decHandler, incHandler) => {
     return div
 }
 
+const renderOrder = (rootElement, order) => {    
+    if (rootElement.children.length > 0) {
+    rootElement.removeChild(rootElement.firstElementChild)    
+    }
+    // HW*: render in better format
+
+    let div = document.createElement('div')
+    let totalText = `Total: ${order.total} €`
+
+    if (order.total > data.promotions.freeDeliveryLimit.limit) {
+        totalText += ` ( Free Delivery )`
+    } else {
+        totalText += ` ( + ${data.promotions.freeDeliveryLimit.deliveryCost} € Delivery )`
+        order.total += data.promotions.freeDeliveryLimit.deliveryCost
+    }
+
+    let discountText = ''
+
+    if (order.items.length >= data.promotions.discount.limit) {        
+        const totalDiscount = order.total * data.promotions.discount.cost
+        const discountedTotal = order.total - totalDiscount
+
+        discountText = `<br>Discount: ${totalDiscount} € 
+                        <br>Discounted Total: ${discountedTotal} €`
+    } else {
+        discountText += `<br>Order Total Cost: ${order.total} €`
+    }
+
+    div.innerHTML = totalText + discountText    
+    rootElement.appendChild(div)
+}
+
 // EVENTS
 const toggleMenuItemHandler = ( event ) => {
     let toggledInput = event.target
     let label = toggledInput.parentElement
+    let id = +toggledInput.dataset.id
 
     if ( toggledInput.checked) {        
-        let menuItemQuantity = renderMenuQuantity(decQuantityHandler, incQuantityHandler)
-        // label.parentElement.insertBefore(div, label.nextElementSibling)
+        let menuItemQuantity = renderMenuItemQuantity(id, decQuantityHandler, incQuantityHandler)
+        
         label.after(menuItemQuantity)
+
+        addItemToOrder( +id )
+
     } else {
+        removeItemFromOrder(id)
         label.parentElement.removeChild(label.nextElementSibling)
     }         
        
 }
 
+// HW*: optimize this code
+
+const updateQuantityHandler = (event) => {
+    let btnElement = event.target
+    let id = +btnElement.dataset.id
+    let selected = data.order.items.find(
+        item => item.itemRef.id === id
+    )
+    return { id, selected }
+}
+
+// HW*: update and render the total
+
+const updateTotal = () => {
+    let total = data.order.items.reduce((acc, item) => acc + item.quantity * item.itemRef.price, 0)
+    data.order.total = total
+
+    renderOrder(footerSection, data.order)
+}
+
 const decQuantityHandler = ( event) => {
-    event.preventDefault()
-    alert()
+    event.preventDefault()    
+    
+    let { id, selected } = updateQuantityHandler(event)      
+    // HW*: try use ternary operator, try to use and/or operator
+
+    // selected.quantity = selected.quantity > 1 ? selected.quantity - 1 : selected.quantity
+    // selected.quantity > 1 && (selected.quantity -= 1)
+
+    if (selected.quantity > 1) {
+        selected.quantity--
+    }
+
+    let input = document.querySelector(`input[data-id="${id}"][type="text"]`)
+    input.value = selected.quantity
+
+    updateTotal()    
 }
 
 const incQuantityHandler = ( event) => {
-    event.preventDefault()
-    alert()
+    event.preventDefault()   
+    
+    let { id, selected } = updateQuantityHandler(event)
+    // HW*: try use ternary operator, try to use and/or operator
+
+    // selected.quantity = selected.quantity < 5 ? selected.quantity + 1 : selected.quantity
+    // selected.quantity < 5 && (selected.quantity += 1)
+
+    if (selected.quantity < 5 ) {
+        selected.quantity++
+    }
+
+    let input = document.querySelector(`input[data-id="${id}"][type="text"]`)
+    input.value = selected.quantity    
+      
+    updateTotal()
 }
-
-
-
-
-
 
 
 //--------------------------------------------------
 const contentSection = document.getElementById("content") // HW*: use another way ----> const contentSection = document.querySelector("#content")
+const footerSection = document.getElementsByTagName("footer")[0]
 
 renderMenuForm(contentSection, data.menu)
+renderOrder(footerSection, data.order)
+
--- 6. DOM__advanced___menu/overview.md
+++ 6. DOM__advanced___menu/overview.md
@@ -137,3 +137,107 @@ DOM events bubbling
              |       \
              |        +----- click ------ incHandler( event )
              |                                event.preventDefault()
+
+
+
+
+
+
+
+
+OBSERVER PATTERN (event handler, pub-sub)
+
+
+<element on[eventname]="eventHandler()">
+   ^
+   |
+  elementVariable.addEventListener('eventname', eventHandler)
+                                                      |
+                                                      v
+                                                  eventHandler () {
+                                                    ...
+                                                  }
+
+
+
+
+
+
+
+
+      DOM
+       |
+       v
+                              +----- Observer -------+
+                              |                      |
+                              |                      |
+                              |  'click'             +----------- > eventHandler ( e ) {
+                              |                      |               ...           ^
+      parentElement           +-----+----------------+             }               |
+       |                            |                                              |
+       .                            |                                              |
+       |                            v                                              |
++------------- <element> -------------+                                            |
+|                                     |  dispatch                                  |
+|                                     |<-------                                    |
+|                                     +---- event {...} ---------------------------+
+|                                     |       ^
++-------------------------------------+       |
+                      ^                       |
+                      |                       |
+                    action -------------------+
+                      |
+                    user (also: op. system, agent, ...)
+
+
+
+
+
+
+!!! TODO: clone + immutable
+
+let data = {
+    menu: {
+        food: [
+            new Food("Pizza", "images/pizza.jpg", 100), <----+
+            new Food("Salad", "images/salad.jpg", 50),       |
+            new Food("Soup", "images/soup.jpeg",  25),       |
+        ],                                                   |
+        drinks: [                                            |
+            // HW1: do the same for drinks                   |
+            new Food("Black Tea", "", 50),                   |
+            new Food("Green Tea", "", 45),                   |
+            new Food("Water",     "", 15),                   |
+        ]                                                    |
+    },                                                       |
+    order: {        +----------------------------------------+
+        items: [
+        { itemRef: ..., quantity: 1 }
+        ]           |
+    }               |
+}                   v
+            new Food("Pizza", "images/pizza.jpg", 100)
+------------------------------------------------------------------------------
+data.order.items.push(
+        {itemRef: data.menu.food[0], quantity: 1}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<button data-size="10" data-style="dark" daat-effect="fade"> 
+</button>
+                 |
+                 |
+                 v
+     element.dataset ---> {   }
